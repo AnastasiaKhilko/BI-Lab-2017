4148b4b3f01b17103b25540f139b12eab4af6c41
lolasova <olga_hilko@epam.com>
2017-11-03 12:51:50 +0300

    Sorry, friends!

--- Olga_Hilko/reports/Task1/0.2.sql
+++ Olga_Hilko/reports/Task1/0.2.sql
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 select  vsize(a) a_Size, vsize(b) b_Size, vsize(c) ?_Size, vsize(a)+vsize(b)+vsize(c) "a+b+c" from t;
 
 select dbms_rowid.rowid_relative_fno(rowid), dbms_rowid.rowid_block_number( rowid)  from t;
@@ -11,3 +12,18 @@ select from t;
 --alter system dump datafile 15 block 133;
 
 
+=======
+select  vsize(a) a_Size, vsize(b) b_Size, vsize(c) ?_Size, vsize(a)+vsize(b)+vsize(c) "a+b+c" from t;
+
+select dbms_rowid.rowid_relative_fno(rowid), dbms_rowid.rowid_block_number( rowid)  from t;
+
+-- 15
+
+select from t;
+
+-- 133
+
+--alter system dump datafile 15 block 133;
+
+
+>>>>>>> e9dba30c18f4d0779e4da81e62ed8e6eccde3b78
