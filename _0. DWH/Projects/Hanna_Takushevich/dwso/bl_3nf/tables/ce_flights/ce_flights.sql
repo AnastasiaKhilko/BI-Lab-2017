7f9b7ddff0e6408a5ecd41eaf408bc7adcbb5836
Hanna_takushevich <Hanna_takushevich@epam.com>
2017-11-22 23:50:34 +0300

    external tables

--- _0. DWH/Projects/Hanna_Takushevich/dwso/bl_3nf/tables/ce_flights/ce_flights.sql
+++ _0. DWH/Projects/Hanna_Takushevich/dwso/bl_3nf/tables/ce_flights/ce_flights.sql
@@ -1,10 +1,13 @@
-CREATE TABLE ce_flights
-  (
-    flight_id           NUMBER,
-    route_id            NUMBER,
-    flight_code         VARCHAR2(10),
-    departure_date_time DATE,
-    duration_minutes    NUMBER,
-    CONSTRAINT ce_flights_pk PRIMARY KEY (flight_id),
-    CONSTRAINT flight_rote_fk FOREIGN KEY (route_id) REFERENCES ce_routes (route_id)
-  );
+CREATE TABLE ce_flights (
+    flight_id             NUMBER,
+    route_id              NUMBER,
+    airline_id            NUMBER,
+    flight_code           VARCHAR2(10),
+    departure_date_time   DATE,
+    duration_minutes      NUMBER,
+    CONSTRAINT ce_flights_pk PRIMARY KEY ( flight_id ),
+    CONSTRAINT flight_rote_fk FOREIGN KEY ( route_id )
+        REFERENCES ce_routes ( route_id ),
+    CONSTRAINT flight_airline_fk FOREIGN KEY ( airline_id )
+        REFERENCES ce_airlines ( airline_id )
+);
--- _0. DWH/Projects/Hanna_Takushevich/dwso/bl_dm/tables/dim_times/dim_times.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dim_times (
-    time_id       NUMBER,
-    hours         NUMBER,
-    minutes       NUMBER,
-    description   VARCHAR2(5),
-    CONSTRAINT dim_times_pk PRIMARY KEY ( time_id )
-);
