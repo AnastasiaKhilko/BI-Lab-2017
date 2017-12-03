0dddf6fbb02640075f0e72b23dcd0a88ba99c6c8
Arina Marchenko <Arina_Marchenko@epam.com>
2017-12-03 20:48:26 +0300

    modified bl_dm

--- _0. DWH/Projects/Arina_Marchenko/dwso/bl_cl_dm/packages/package_dim.sql
+++ _0. DWH/Projects/Arina_Marchenko/dwso/bl_cl_dm/packages/package_dim.sql
@@ -46,18 +46,18 @@ commit;
   BEGIN
     EXECUTE IMMEDIATE 'truncate table bl_dm.dim_date';
 	INSERT INTO  bl_dm.dim_date
-SELECT SYSDATE+rownum-365*1018 as Date_id, 
-    to_number(to_char(SYSDATE+rownum-365*1018-7*1, 'D'))as Day_per_week,
-    to_number(to_char(extract(day from SYSDATE+rownum-365*1018)))as Day_per_month,
-    to_number(to_char(SYSDATE+rownum-365*1018, 'DDD')) AS Day_per_year,
-    to_number(to_char(SYSDATE+rownum-365*1018, 'W')) AS Week_per_month,
-    to_number(to_char(SYSDATE+rownum-365*1018, 'IW')) AS Week_per_year,
-    to_number(to_char(extract(month from SYSDATE+rownum-365*1018))) as Month_number,
-    to_char(SYSDATE+rownum-365*1018, 'MONTH', 'NLS_DATE_LANGUAGE=English') AS Month_name,
-    to_number(to_char(extract(year from SYSDATE+rownum-365*1018))) as Year,
-    to_char(extract(day from SYSDATE+rownum-365*1018) || '-'|| to_char(extract(month from (SYSDATE+rownum-365*1018)))) as Day_Month,
-    to_char(extract(year from SYSDATE+rownum-365*1018) || '-'|| to_char(extract(month from (SYSDATE+rownum-365*1018)))) as Year_Month
-FROM dual CONNECT BY rownum <=365*1018;
+  SELECT trunc(SYSDATE)+rownum-365*1019 as Date_id, 
+    to_number(to_char(SYSDATE+rownum-365*1019-7*1, 'D'))as Day_per_week,
+    to_number(to_char(extract(day from SYSDATE+rownum-365*1019)))as Day_per_month,
+    to_number(to_char(SYSDATE+rownum-365*1019, 'DDD')) AS Day_per_year,
+    to_number(to_char(SYSDATE+rownum-365*1019, 'W')) AS Week_per_month,
+    to_number(to_char(SYSDATE+rownum-365*1019, 'IW')) AS Week_per_year,
+    to_number(to_char(extract(month from SYSDATE+rownum-365*1019))) as Month_number,
+    to_char(SYSDATE+rownum-365*1019, 'MONTH', 'NLS_DATE_LANGUAGE=English') AS Month_name,
+    to_number(to_char(extract(year from SYSDATE+rownum-365*1019))) as Year,
+    to_char(extract(day from SYSDATE+rownum-365*1019) || '-'|| to_char(extract(month from (SYSDATE+rownum-365*1019)))) as Day_Month,
+    to_char(extract(year from SYSDATE+rownum-365*1019) || '-'|| to_char(extract(month from (SYSDATE+rownum-365*1019)))) as Year_Month
+FROM dual CONNECT BY rownum <=365*1019;
 	commit;
   EXCEPTION
   WHEN OTHERS THEN
@@ -166,7 +166,7 @@ SELECT car_id,
     maxPrice,
     avgPrice,
     sdPrice,
-    start_dt
+    trunc(start_dt)
 FROM cl_fct_orders;
 commit;
   EXCEPTION
@@ -175,3 +175,4 @@ commit;
   END load_fct_orders;
 END pkg_etl_load_dim_cars;
 /
+
